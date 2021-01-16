6be85c1cac9bfab01555fa1bb234f088daa4f619
Jose Justin <joselsjustin@gmail.com>
2021-01-16 19:21:37 -0300

    Backend - Change express settings
    Add login routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import { userExpressRouter } from './components/user';
+import { loginExpressRouter } from './components/login';
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/users', userExpressRouter);
+app.use('/login', loginExpressRouter);
 
 export default app;
