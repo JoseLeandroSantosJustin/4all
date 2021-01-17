ce98d5a24ae9fcea45cf6c186ee15d09a2cc1022
Jose Justin <joselsjustin@gmail.com>
2021-01-17 17:56:04 -0300

    Backend - Change express settings
    Add login authentication middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,17 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import { userExpressRouter } from './components/user';
-import { loginExpressRouter } from './components/login';
+import {
+  loginExpressRouter,
+  authenticationExpressRouter
+} from './components/login';
 import { rentalStoreExpressRouter } from './components/rental_store';
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/users', userExpressRouter);
 app.use('/login', loginExpressRouter);
+app.use(authenticationExpressRouter);
 app.use('/rental-store', rentalStoreExpressRouter);
 
 export default app;
