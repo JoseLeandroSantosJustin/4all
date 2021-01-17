eb32aa5b2596b4604b12185dd0d045b6c9c29100
Jose Justin <joselsjustin@gmail.com>
2021-01-17 16:04:21 -0300

    Backend - Change express settings
    Add rental-store routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import { userExpressRouter } from './components/user';
 import { loginExpressRouter } from './components/login';
+import { rentalStoreExpressRouter } from './components/rental_store';
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/users', userExpressRouter);
 app.use('/login', loginExpressRouter);
+app.use('/rental-store', rentalStoreExpressRouter);
 
 export default app;
